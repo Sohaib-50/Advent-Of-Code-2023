5789a627a43919205133cbc3eaee2e45dbf5f9c5
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2023-12-06 11:46:00 +0500

    completed day6 part2

--- day5/solution2_retry.py
+++ day5/solution2_retry.py
@@ -1,40 +1,50 @@
-with open("test_case.txt") as f:
-    seeds_data = [int(i) for i in f.readline().split(": ")[1].split()]  
-
-    current_intervals = []
-    for i in range(0, len(seeds_data), 2):
-        current_intervals.append((seeds_data[i], seeds_data[i] + seeds_data[i + 1])) 
-
-    data = f.read().strip()
-
-
-maps_collections = [x.split(":\n")[1].strip() for x in data.split("\n\n")]
-maps_collections = [x.split("\n") for x in maps_collections]
-print(maps_collections)
+with open("input.txt") as f:
+    data = f.read().split("\n\n")
+    line1 = [int(x) for x in data[0].strip().split()[1:]]
+    ranges = []
+    for i in range(0, len(line1), 2):
+        ranges.append((line1[i], line1[i] + line1[i + 1]))
+    maps = [x.split(":\n")[1]  for x in data[1:]]
+    maps = [x.split("\n")  for x in maps]
 
 
-for map_block in maps_collections:
-    temp_intervals = []
+# for each map type
+for map_block in maps:
+    temp = []
 
-    while current_intervals:
-        interval_start, interval_end = [int(x) for x in current_intervals.pop()]
+    # pass each seed (range) through each map in current map type
+    # check each range against each mapping and try to map
+    while ranges:
+        range_start, range_end = ranges.pop()
+
+        # for each map in the current map type
         for map_ in map_block:
-            src, dest, l = [int(x) for x in (map_.split(" "))]
-            overlap_start = min(interval_start, dest)
-            overlap_end = max(interval_end, dest + l)
+            dest, src, l = list(map(int, map_.split()))
 
-            if overlap_start < overlap_end:
-                temp_intervals.append((overlap_start - dest + src, overlap_end - dest + src))
+            overlap_start = max(src, range_start)
+            overlap_end = min(src + l, range_end)
 
-                if overlap_start > interval_start:
-                    current_intervals.append((interval_start, overlap_start))
-                if overlap_end < interval_end:
-                    current_intervals.append((overlap_end, interval_end))
+            # if the seed range we are checking overlaps (partially or fully) with the map we're checking
+            # then find the translated region (after mapping) of the overlap, and add it 
+            if overlap_end > overlap_start:
+                translated_overlap_start = dest + (overlap_start - src)
+                translated_overlap_end = dest + (overlap_end - src)
+                temp.append((translated_overlap_start, translated_overlap_end))
+
+                # add rest of the non overlapping parts if any back to ranges for further processing
+                if overlap_start > range_start:
+                    ranges.append((range_start, overlap_start))
+
+                if overlap_end < range_end:
+                    ranges.append((overlap_end, range_end))
+                
+                # if match was found for current range we can stop checking other maps
                 break
-        else:
-            temp_intervals.append((interval_start, interval_end))
 
+        else:  # if no match found for current range, directly add to temp since its a 1:1 mapping
+            temp.append((range_start, range_end))
+        
+    ranges = temp
+        
+print(sorted(ranges)[0][0])
 
-    current_intervals = temp_intervals
-
-print(current_intervals)
